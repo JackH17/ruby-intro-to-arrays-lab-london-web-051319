29ef866115ed7876f249824a6696412f3c935296
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 16:23:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,4 +18,5 @@ def first_element(my_first_element)
   my_first_element = @taylor_swift.first 
 end
 
-def 
+def third_element
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
